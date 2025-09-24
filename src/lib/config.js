6ba3cd1f3e55c6388ae5c4544be90dee223034c7
Adrien Revel <adrien.revel@gmail.com>
2025-09-24 13:59:18 +0200

    fix: update version to 0.4.0-beta.4 in package.json and config.js

--- src/lib/config.js
+++ src/lib/config.js
@@ -2,7 +2,7 @@ export const siteConfig = {
 	title: 'microfolio',
 	description: 'static portfolio generator',
 	author: 'AKER',
-	version: '0.4.0-beta.3',
+	version: '0.4.0-beta.4',
 
 	// Social links
 	socialLinks: {

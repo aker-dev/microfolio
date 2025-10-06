01b9edb5c656b49bf220cee65ad411d0c0a668c9
Adrien Revel <adrien.revel@gmail.com>
2025-10-06 21:22:57 +0200

    chore: update version to 0.5.0-beta.4 in site configuration

--- src/lib/config.js
+++ src/lib/config.js
@@ -2,7 +2,7 @@ export const siteConfig = {
 	title: 'microfolio',
 	description: 'static portfolio generator',
 	author: 'AKER',
-	version: '0.5.0-beta.3',
+	version: '0.5.0-beta.4',
 
 	// Internationalization config
 	locale: 'en', // 'en', 'fr'
